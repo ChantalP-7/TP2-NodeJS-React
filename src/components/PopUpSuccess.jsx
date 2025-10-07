991c2a7d6470e751a490d3bb2a0f5f05f6873cff
= <chanmichou37@gmail.com>
2025-10-07 19:36:10 -0400

    Ajout d'un popup de succès

---
+++ src/components/PopUpSuccess.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+const PopUpSuccess = ({ message, onClose }) => {
+	const [visible, setVisible] = useState(false);
+
+	useEffect(() => {
+		if (message) {
+			// Fade in
+			const showTimer = setTimeout(() => {
+				setVisible(true);
+			}, 100); // petit délai pour forcer l’animation
+
+			// Fade out après 5s
+			const hideTimer = setTimeout(() => {
+				setVisible(false);
+				// Supprimer le message après l’animation
+				setTimeout(onClose, 500);
+			}, 8000);
+
+			return () => {
+				clearTimeout(showTimer);
+				clearTimeout(hideTimer);
+			};
+		}
+	}, [message, onClose]);
+
+	if (!message) return null;
+
+	return (
+		<div
+			className={`fixed top-6 right-6 bg-green-500 text-white px-4 py-2 rounded shadow-lg z-50 max-w-xs transition-opacity duration-500 ease-in-out
+				${visible ? "opacity-100" : "opacity-0"}
+			`}
+		>
+			{message}
+			<button
+				onClick={() => {
+					setVisible(false);
+					setTimeout(onClose, 500);
+				}}
+				className="ml-4 font-bold"
+			>
+				X
+			</button>
+		</div>
+	);
+};
+
+export default PopUpSuccess;
